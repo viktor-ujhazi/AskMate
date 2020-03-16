f236783d73fa409c01d3b37a075490eb6d745731
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-03-16 11:04:50 +0100

    create question and answer model

---
+++ AskMateMVC/Models/AnswerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMateMVC.Models
+{
+    public class AnswerModel
+    {
+        public int ID { get; set; }     //id: A unique identifier for the answer
+        public DateTime TimeOfAnswer { get; set; }  //submission_time: The UNIX timestamp when the answer was posted
+        
+        public int VoteNumber { get; set; } //vote_number: The sum of votes this answer has received
+        public int  QuestionID { get; set; }    //question_id: The id of the question this answer belongs to.
+        public string Message { get; set; } //message: The answer text
+        public string Image { get; set; }   //image: the path to the image for this answer
+
+    }
+}
---
+++ AskMateMVC/Models/QuestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMateMVC.Models
+{
+    public class QuestionModel
+    {
+        public int ID { get; set; }     //id: A unique identifier for the question
+        public DateTime TimeOfAnswer { get; set; }  //submission_time: The UNIX timestamp when the question was posted
+        public int ViewNumber { get; set; } //view_number: How many times this question was displayed in the single question view
+        public int VoteNumber { get; set; } //vote_number: The sum of votes this question has received
+        public string Title { get; set; }   //title: The title of the question
+        public string Message { get; set; } //message: The question text
+        public string Image { get; set; }   //image: The path to the image for this question
+        
+        
+    }
+}
