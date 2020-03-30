079303d7b33530c3cf50ecff58c9f3425b5e6eca
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-03-30 11:13:27 +0200

    prepare for database

--- AskMateMVC/Services/IDataHandler.cs
+++ AskMateMVC/Services/IDataHandler.cs
@@ -11,15 +11,15 @@ namespace AskMateMVC.Services
         List<QuestionModel> GetQuestions();
         List<AnswerModel> GetAnswers();
         public void AddQuestion(QuestionModel model);
-        public void AddAnswer(AnswerModel model);
+        public void AddAnswer(AnswerModel model, int id);
         void SaveQuestions();
         void SaveAnswers();
-        QuestionModel GetQuestionByID(Guid id);
-        AnswerModel GetAnswerByID(Guid id);
+        QuestionModel GetQuestionByID(int id);
+        AnswerModel GetAnswerByID(int id);
         //public List<AnswerModel> GetAnswersForQuestion(Guid id);
-        public void RemoveQuestionById(Guid id);
+        public void RemoveQuestionById(int id);
         //public void RemoveAnswersForQuestion(Guid id);
-        public void RemoveAnswer(Guid id);
+        public void RemoveAnswer(int id);
         //List<QuestionModel> MostViewedQuestions();
     }
 }
