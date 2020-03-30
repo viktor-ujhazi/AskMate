61dfac1acd04d7ef17d000cdeda119ba587e3568
Adam <borbelya90@gmail.com>
2020-03-30 15:07:25 +0200

    SQL table skeleton updated

--- AskMate.sql
+++ AskMate.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS Questions CASCADE;
+DROP TABLE IF EXISTS Answers CASCADE;
+DROP TABLE IF EXISTS Comment_s CASCADE;
+DROP TABLE IF EXISTS Tag CASCADE;
+DROP TABLE IF EXISTS Question_tag CASCADE;
+
 CREATE TABLE IF NOT EXISTS Questions(
 	question_id SERIAL PRIMARY KEY,
 	question_time Date,
@@ -12,8 +18,28 @@ CREATE TABLE IF NOT EXISTS Questions(
 CREATE TABLE IF NOT EXISTS Answers(
 	answer_id SERIAL PRIMARY KEY,
 	answer_time DATE,
-    answer_voteNumber INT,
-	question_id INT REFERENCES Questions(question_id),
+    	answer_voteNumber INT,
+	question_id INT REFERENCES Questions(question_id) ON DELETE CASCADE,
 	answer_message TEXT,
 	answer_image TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Comment_s(
+	comment_id SERIAL PRIMARY KEY,
+	question_id INT REFERENCES Questions(question_id) ON DELETE CASCADE,
+	answer_id INT REFERENCES Answers(answer_id) ON DELETE CASCADE,
+	comment_message TEXT,
+	comment_time DATE,
+	edited_number INT
+);
+
+CREATE TABLE IF NOT EXISTS Tags(
+	tag_id SERIAL PRIMARY KEY,
+	tag_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Question_tags(
+	question_id INT REFERENCES Questions(question_id) ON DELETE CASCADE,
+	tag_id INT REFERENCES Tags(tag_id),
+	PRIMARY KEY (question_id, tag_id)
 );
